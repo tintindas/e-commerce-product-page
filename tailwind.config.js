886b0068628a49ff2f407cbcf92952e169d7ee5a
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-03-10 22:49:48 +0530

    add custom theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-	path: ['./src/**/*.{html,js}'],
-	content: [],
+	content: ['./index.html'],
 	theme: {
+		colors: {
+			orange: '#ff7d1a',
+			'pale-orange': '#ffede0',
+			'very-dark-blue': '#1d2025',
+			'dark-grayish-blue': '#68707d',
+			'grayish-blue': '#b6bcc8',
+			'light-grayish-blue': '#f7f8fd'
+		},
+		fontFamily: {
+			sans: 'Kumbh Sans'
+		},
 		extend: {}
 	},
 	plugins: []
