a5467b14a0cf91b0d32cf757b8bcac6ede3d92d3
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-03-13 08:57:19 +0530

    implement infinite horizontal slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 const arrowLeft = document.getElementById('arrow-left')
 const arrowRight = document.getElementById('arrow-right')
+const slider = document.getElementById('slider')
+const images = document.getElementsByClassName('image-container')
 const minus = document.getElementById('minus')
 const plus = document.getElementById('plus')
 const quantity = document.getElementById('quantity')
+
+// Slider
+let currentIndex = 1
+let isMoving = false
+
+const moveSlider = () => {
+	isMoving = true
+	slider.style.transform = `translateX(-${currentIndex * 100}vw)`
+}
+
+arrowLeft.addEventListener('click', () => {
+	if (isMoving) return
+	currentIndex--
+	moveSlider()
+})
+
+arrowRight.addEventListener('click', () => {
+	if (isMoving) return
+	currentIndex++
+	moveSlider()
+})
+
+slider.addEventListener('transitionend', () => {
+	isMoving = false
+	if (currentIndex === 0) {
+		currentIndex = images.length - 2
+		slider.style.transitionDuration = '1ms'
+		return moveSlider()
+	}
+	if (currentIndex === images.length - 1) {
+		currentIndex = 1
+		slider.style.transitionDuration = '1ms'
+		return moveSlider()
+	}
+	slider.style.transitionDuration = '300ms'
+})
+
+// Quantity
 
 let quant = parseInt(quantity.innerHTML)
 
